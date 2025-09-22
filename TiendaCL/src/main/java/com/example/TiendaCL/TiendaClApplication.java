77e840dca7a4ff49f9c032c1ad0fbb17706495db
CarlosL0pez <carloslopezarias.21@gmail.com>
2025-09-21 19:39:28 -0600

    Class1
    Se realizó hasta el ejemplo 2 de multimedia de autos

---
+++ TiendaCL/src/main/java/com/example/TiendaCL/TiendaClApplication.java
@@ -0,0 +1,13 @@
+package com.example.TiendaCL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaClApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaClApplication.class, args);
+	}
+
+}
